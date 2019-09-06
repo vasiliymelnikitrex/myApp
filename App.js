547829c98804a6739bbbdb725d184f9f73a8d6cf
Vasiliy <vasiliy.melnik@itrexgroup.com>
2019-09-06 14:57:43 +0300

    Pre commit test 1

--- App.js
+++ App.js
@@ -7,21 +7,15 @@
  */
 
 import React, { Fragment } from 'react';
-import {
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
+import { View, Text, StatusBar } from 'react-native';
 
 const App = () => (
   <Fragment>
     <StatusBar barStyle="dark-content" />
+
     <View>
-            <Text>
-       My App
-      </Text>
-      </View>
+      <Text>My App</Text>
+    </View>
   </Fragment>
 );
 
