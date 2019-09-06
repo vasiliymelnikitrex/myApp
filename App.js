eecc29c88d9a62a1e14f12d9e34578e395cc3fc9
Vasiliy <vasiliy.melnik@itrexgroup.com>
2019-09-06 14:36:05 +0300

    Initial

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => (
+  <Fragment>
+    <StatusBar barStyle="dark-content" />
+    <View>
+          <Text>
+      My App
+      </Text>
+      </View>
+  </Fragment>
+);
+
+export default App;
