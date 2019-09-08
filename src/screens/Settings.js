f6e597b5ebc0ecb65b9c4994937096efe0f41b23
VasiliyMelnik64 <vasiliymelnik64@gmail.com>
2019-09-08 20:47:53 +0300

    Added tabbar

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default () => (
-  <View style={{ backgroundColor: '#d8f6ff', flex: 1 }}>
+  <View style={{ backgroundColor: '#e1ffe3', flex: 1 }}>
     <Text style={{ color: '#8da645', fontSize: 36, fontWeight: 'bold' }}>
-      List
+    Settings
     </Text>
   </View>
 );
