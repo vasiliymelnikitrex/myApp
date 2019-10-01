575f917e04aeb0ddf839f70f25f1d00cd6683c26
Vasiliy Melnik <vasiliy.melnik@itrexgroup.com>
2019-10-01 09:16:06 +0300

    Add color theme

--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -4,7 +4,6 @@ import { Container } from '../components/Container';
 import { HEADER_FONT_SIZE } from '../styles/fontSizes';
 import { PRIMARY_COLOR } from '../styles/colors';
 
-
 export default () => (
   <Container>
     <Text style={{ color: PRIMARY_COLOR, fontSize: HEADER_FONT_SIZE, fontWeight: 'bold' }}>
